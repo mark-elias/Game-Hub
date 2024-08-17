28f1cc3826bf21628290ecc30fe0f4a64e93c447
mark-elias <mark7elias@gmail.com>
2024-08-17 05:34:40 -0700

    clean up the GameCard

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,8 +17,8 @@ function GameGrid({ gameQuery }: Props) {
     <div>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 3 }}
-        spacing={3}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={6}
         padding="10px"
       >
         {isLoading &&
